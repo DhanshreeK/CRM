20695c0e8c36538352e7265173d93bc2cf411d98
Dhanshree Kamble <dhanshree.j.kamble@gmail.com>
2017-01-23 12:57:20 +0530

    finished the missing email and alphabetized actions they now use the index view

--- app/controllers/finder_controller.rb
+++ app/controllers/finder_controller.rb
@@ -1,7 +1,21 @@
 class FinderController < ApplicationController
 	def index
+		@page_title = "All Customers"
 		@customers = Customer.all
 	end
 	# the @customers variable will now be acessible from
 	# the automatically loaded view: app/views/finder/index.html.erb
+
+
+	def alphabetized
+		@page_title = "Alphabetized customers"
+		@customers = Customer.order(:full_name)
+		render :index
+	end
+
+	def missing_email
+		@page_title = "Missing_email customers"
+		@customers = Customer.where(email_address:'')
+		render :index
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  root to: 'finder#index'
+
+ get 'alphabetized' => 'finder#alphabetized', as: 'alphabetized'
+ get 'missing_email' => 'finder#missing_email', as: 'missing_email'
 end
